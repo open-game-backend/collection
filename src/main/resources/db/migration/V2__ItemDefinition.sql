b9a8c08dc6101706279ce7f05da821a8bdfa8e37
Nick Prühs <dev@npruehs.de>
2021-01-20 19:04:45 +0100

    Fix foreign key columns to match expected names.

--- src/main/resources/db/migration/V2__ItemDefinition.sql
+++ src/main/resources/db/migration/V2__ItemDefinition.sql
@@ -1,7 +1,7 @@
 CREATE TABLE collection_itemdefinition (
     id VARCHAR(100) NOT NULL,
-    item_type VARCHAR(100) NULL,
+    item_type_type VARCHAR(100) NULL,
 
     PRIMARY KEY (id),
-    FOREIGN KEY (item_type) REFERENCES collection_itemtype(type)
+    FOREIGN KEY (item_type_type) REFERENCES collection_itemtype(type)
 );
--- src/main/resources/db/migration/V3__CollectionItem.sql
+++ src/main/resources/db/migration/V3__CollectionItem.sql
@@ -1,11 +1,11 @@
 CREATE TABLE collection_item (
     id INT NOT NULL AUTO_INCREMENT,
     player_id VARCHAR(100) NOT NULL,
-    item_definition VARCHAR(100) NOT NULL,
+    item_definition_id VARCHAR(100) NOT NULL,
     count INT(10) UNSIGNED NOT NULL,
 
     PRIMARY KEY (id),
-    FOREIGN KEY (item_definition) REFERENCES collection_itemdefinition(id)
+    FOREIGN KEY (item_definition_id) REFERENCES collection_itemdefinition(id)
 );
 
 CREATE INDEX ix_collection_item_player_id ON collection_item (player_id);
