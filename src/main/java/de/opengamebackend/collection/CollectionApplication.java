54bb9b7688d7e79e700bfb2367d28ab3896aa1fc
Nick Prühs <dev@npruehs.de>
2021-01-21 22:43:23 +0100

    Setup Elastic logging.

--- src/main/java/de/opengamebackend/collection/CollectionApplication.java
+++ src/main/java/de/opengamebackend/collection/CollectionApplication.java
@@ -1,13 +1,15 @@
 package de.opengamebackend.collection;
 
+import de.opengamebackend.util.EnableOpenGameBackendUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
+@EnableOpenGameBackendUtils
 public class CollectionApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CollectionApplication.class, args);
 	}
-
 }
