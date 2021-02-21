8fb517f7642849dd944b033cbd597c7d53c178ee
Nick Prühs <dev@npruehs.de>
2021-02-21 12:41:58 +0100

    Fix foreign key column names in LoadoutRule table.

--- src/main/resources/db/migration/V8__LoadoutRule.sql
+++ src/main/resources/db/migration/V8__LoadoutRule.sql
@@ -1,12 +1,12 @@
 CREATE TABLE collection_loadoutrule (
     id INT NOT NULL AUTO_INCREMENT,
-    type VARCHAR(100) NOT NULL,
-    item_tag VARCHAR(100) NOT NULL,
+    type_id VARCHAR(100) NOT NULL,
+    item_tag_tag VARCHAR(100) NOT NULL,
     min_total INT(32) UNSIGNED NULL,
     max_total INT(32) UNSIGNED NULL,
     max_copies INT(32) UNSIGNED NULL,
 
     PRIMARY KEY (id),
-    FOREIGN KEY (type) REFERENCES collection_loadouttype(id),
-    FOREIGN KEY (item_tag) REFERENCES collection_itemtag(tag)
+    FOREIGN KEY (type_id) REFERENCES collection_loadouttype(id),
+    FOREIGN KEY (item_tag_tag) REFERENCES collection_itemtag(tag)
 );
